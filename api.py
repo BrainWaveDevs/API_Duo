2d012c4fd58a2c2bdb25fc5a020c8c732a5586a0
TcheloBorgas <marcelo.borgas@edu.fecap.br>
2023-03-23 18:17:20 -0300

    first commit

---
+++ api.py
@@ -0,0 +1,35 @@
+from flask import Flask, jsonify, request
+import unicodedata
+
+app = Flask(__name__)
+
+# Lista integrada com alguns alunos
+alunos = ['Joao', 'Maria', 'Pedro', 'Ana']
+
+# Dicionário para armazenar a lista de presença
+presenca = {}
+
+# Função para substituir caracteres especiais por equivalentes sem acentos
+def remover_acentos(texto):
+    return unicodedata.normalize('NFKD', texto).encode('ASCII', 'ignore').decode('ASCII')
+
+# Rota para obter a lista de alunos
+@app.route('/alunos', methods=['GET'])
+def get_alunos():
+    return jsonify(alunos)
+
+# Rota para marcar a presença de um aluno
+@app.route('/presenca', methods=['PUT'])
+def update_presenca():
+    aluno = request.json['aluno']
+    presente = request.json['presente']
+    presenca[remover_acentos(aluno)] = presente
+    return jsonify(presenca)
+
+# Rota para obter a lista de presença
+@app.route('/presenca', methods=['GET'])
+def get_presenca():
+    return jsonify(presenca)
+
+if __name__ == '__main__':
+    app.run(debug=True)
